5d36d48bf1a76ac4da893165e35634f388507cc3
mskorotkov <mskorotkov@mail.ru>
2023-07-15 15:50:06 +0300

    Update 1_22.cpp

--- ch01/1_22.cpp
+++ ch01/1_22.cpp
@@ -5,20 +5,31 @@ using namespace std;
 
 int main() {
     Sales_item total;
+
     if (cin >> total) {
         Sales_item book;
         while (cin >> book) {
             if (total.isbn() == book.isbn()) {
                 total += book;
             } else {
-                cout << total;
-                total = book;
+                cerr << "Data must refer to same ISBN" << endl;
+                return -1;
             }
         }
-        cout << total;
     } else {
-        cerr << "No data" << endl;;
+        cerr << "No data!" << endl;;
         return -1;
     }
+    cout << total;
     return 0;
 }
+
+/*
+Input:
+0-123-12345-X 1 30.0
+0-123-12345-X 2 20.0
+0-123-12345-X 3 10.0
+
+Output:
+0-123-12345-X 6 100 16.6667
+*/
