0c2965108eafec4cec2320ba250f38568dba1ba8
mskorotkov <mskorotkov@mail.ru>
2023-07-17 00:02:49 +0300

    Update 2_24.cpp

--- ch02/2_24.cpp
+++ ch02/2_24.cpp
@@ -1,6 +1,6 @@
 int main() {
     int i = 42;
     void *p = &i;   // [OK] a void pointer is a special pointer that can point to objects of any data type
-    long *lp = &i;  // [Error] invalid conversion from 'int*' to 'long int*' [-fpermissive]
+    long *lp = &i;  // [Error] invalid conversion from 'int*' to 'long int*'
     return 0;
 }
