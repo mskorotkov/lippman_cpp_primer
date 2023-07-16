c6b60b2e65fdc6f24ba2e4bb62f0199ca33be146
mskorotkov <mskorotkov@mail.ru>
2023-07-16 13:53:38 +0300

    Update 2_15.cpp

--- ch02/2_15.cpp
+++ ch02/2_15.cpp
@@ -1,7 +1,7 @@
 int main() {
-    int ival = 1.01;    // OK
-    int &rval1 = 1.01;  // [Error] cannot bind non-const lvalue reference of type 'int&' to an rvalue of type 'int'
-    int &rval2 = ival;  // OK
-    int &rval3;         // [Error] 'rval3' declared as reference but not initialized
+    int ival = 1.01;    // a) OK
+    int &rval1 = 1.01;  // b) [Error] cannot bind non-const lvalue reference of type 'int&' to an rvalue of type 'int'
+    int &rval2 = ival;  // c) OK
+    int &rval3;         // d) [Error] 'rval3' declared as reference but not initialized
     return 0;
 }
