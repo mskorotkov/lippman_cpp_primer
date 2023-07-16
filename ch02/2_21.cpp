0b0c08a962f8c45c6bcf7c80dc635344b55ac155
mskorotkov <mskorotkov@mail.ru>
2023-07-16 13:52:43 +0300

    Update 2_21.cpp

--- ch02/2_21.cpp
+++ ch02/2_21.cpp
@@ -1,7 +1,7 @@
 int main() {
     int i = 0;
-    double* dp = &i;  // [Error] cannot convert 'int*' to 'double*' in initialization
-    int *ip = i;      // [Error] invalid conversion from 'int' to 'int*' [-fpermissive]
-    int *p = &i;      // OK
+    double* dp = &i;  // a) [Error] cannot convert 'int*' to 'double*' in initialization
+    int *ip = i;      // b) [Error] invalid conversion from 'int' to 'int*' [-fpermissive]
+    int *p = &i;      // c) OK
     return 0;
 }
