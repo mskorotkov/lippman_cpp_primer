abd59e5c85ea862889da17f6e431b0f987ed9ce0
mskorotkov <mskorotkov@mail.ru>
2023-07-17 00:02:38 +0300

    Update 2_26.cpp

--- ch02/2_26.cpp
+++ ch02/2_26.cpp
@@ -1,5 +1,5 @@
 int main() {
-    const int buf;       // a) [Error] uninitialized const 'buf' [-fpermissive]
+    const int buf;       // a) [Error] uninitialized const 'buf'
     int cnt = 0;         // b) OK
     const int sz = cnt;  // c) OK
     ++cnt;               // d) OK
