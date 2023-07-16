cb6055f17e2658685a7e73ff95ff639c93beae4d
mskorotkov <mskorotkov@mail.ru>
2023-07-16 14:09:03 +0300

    Create 2_24.cpp

---
+++ ch02/2_24.cpp
@@ -0,0 +1,6 @@
+int main() {
+    int i = 42;
+    void *p = &i;   // [OK] a 'void *' pointer can point to any type
+    long *lp = &i;  // [Error] invalid conversion from 'int*' to 'long int*' [-fpermissive]
+    return 0;
+}
