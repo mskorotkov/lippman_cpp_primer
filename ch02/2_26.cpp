b46d7b8f1c9c4330d417c27ee827e18dffe1a32d
mskorotkov <mskorotkov@mail.ru>
2023-07-16 22:15:41 +0300

    Create 2_26.cpp

---
+++ ch02/2_26.cpp
@@ -0,0 +1,8 @@
+int main() {
+    const int buf;       // a) [Error] uninitialized const 'buf' [-fpermissive]
+    int cnt = 0;         // b) OK
+    const int sz = cnt;  // c) OK
+    ++cnt;               // d) OK
+    ++sz;                // d) [Error] increment of read-only variable 'sz'
+    return 0;
+}
