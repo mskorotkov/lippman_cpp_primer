2f65893ba979dc2a6db164ec619aa6a281d9e623
mskorotkov <mskorotkov@mail.ru>
2023-07-17 00:04:06 +0300

    Update 2_28.cpp

--- ch02/2_28.cpp
+++ ch02/2_28.cpp
@@ -1,8 +1,8 @@
 int main() {
-    int i, *const cp;       // OK, [Error] uninitialized const 'cp'
-    int *p1, *const p2;     // OK, [Error] uninitialized const 'p2'
-    const int ic, &r = ic;  // [Error] uninitialized const 'ic', OK
-    const int *const p3;    // [Error] uninitialized const 'p3'
-    const int *p;           // OK
+    int i, *const cp;       // a) OK, [Error] uninitialized const 'cp'
+    int *p1, *const p2;     // b) OK, [Error] uninitialized const 'p2'
+    const int ic, &r = ic;  // c) [Error] uninitialized const 'ic', OK
+    const int *const p3;    // d) [Error] uninitialized const 'p3'
+    const int *p;           // e) OK
     return 0;
 }
