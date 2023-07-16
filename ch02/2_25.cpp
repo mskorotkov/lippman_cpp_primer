f9882007218f9211dfefe7fc396da6f2a2eee8dc
mskorotkov <mskorotkov@mail.ru>
2023-07-16 15:54:45 +0300

    Create 2_25.cpp

---
+++ ch02/2_25.cpp
@@ -0,0 +1,16 @@
+int main() {
+    int* ip, i, &r = i;
+    // 'ip' is a pointer to int
+    // 'i' is an integer
+    // 'r' is a reference to the integer 'i'
+
+    int i, *ip = 0;
+    // 'i' is an integer
+    // 'ip' is a pointer to int with NULL value
+
+    int* ip, ip2;
+    // 'ip' is a pointer to int
+    // 'ip2' is an integer
+
+    return 0;
+} 
