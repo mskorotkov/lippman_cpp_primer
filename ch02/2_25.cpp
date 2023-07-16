5415a0c49b24862e68b130676258018d8db2b26c
mskorotkov <mskorotkov@mail.ru>
2023-07-16 15:57:11 +0300

    Update 2_25.cpp

--- ch02/2_25.cpp
+++ ch02/2_25.cpp
@@ -1,16 +1,25 @@
+#include <iostream>
+
 int main() {
-    int* ip, i, &r = i;
-    // 'ip' is a pointer to int
-    // 'i' is an integer
-    // 'r' is a reference to the integer 'i'
+    {
+        int* ip, i, &r = i;
+        // 'ip' is a pointer to int
+        // 'i' is an integer
+        // 'r' is a reference to the integer 'i'
+    }
 
-    int i, *ip = 0;
-    // 'i' is an integer
-    // 'ip' is a pointer to int with NULL value
+    {
+        int i, *ip = 0;
+        // 'i' is an integer
+        // 'ip' is a pointer to int with NULL value
+    }
 
-    int* ip, ip2;
-    // 'ip' is a pointer to int
-    // 'ip2' is an integer
+    {
+        int* ip, ip2;
+        // 'ip' is a pointer to int
+        // 'ip2' is an integer
+    }
 
     return 0;
-} 
+}
+
