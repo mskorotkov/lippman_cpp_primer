48788a9c117581d486d1b19cd53e853e0eff396a
mskorotkov <mskorotkov@mail.ru>
2023-12-15 11:59:52 +0300

    Update 2_18.cpp

--- ch02/2_18.cpp
+++ ch02/2_18.cpp
@@ -1,8 +1,8 @@
 int main() {
-    int x = 777;
-    int y = 3;
-    int *p = &x;  // p = 0x7afe14
-    p = &y;       // p = 0x7afe10
-    *p *= 5;      // y = 15
-    return 0;
+	int x = 777;
+	int y = 3;
+	int *p = &x;  // p = 000000A20D5BFA64, *p = 777
+	p = &y;       // p = 000000ED35CFF984, *p = 3	
+	*p *= 5;      // p = 000000ED35CFF984, *p = 15
+	return 0;
 }
